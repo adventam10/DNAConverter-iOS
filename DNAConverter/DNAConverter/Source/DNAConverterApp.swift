9b7dd7e68235c2b2d9268485e1b393b445ef1848
am10 <adventam10@gmail.com>
2023-06-17 17:52:55 +0900

    Fix app version check

--- DNAConverter/DNAConverter/Source/DNAConverterApp.swift
+++ DNAConverter/DNAConverter/Source/DNAConverterApp.swift
@@ -10,6 +10,22 @@ import SwiftUI
 
 @main
 struct DNAConverterApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    init() {
+        let appStoreManeger = AppStoreManager()
+        appStoreManeger.checkVersion { result in
+            DispatchQueue.main.async {
+                if case .success(.shouldUpdate) = result {
+                    if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+                       let root = windowScene.windows.first?.rootViewController {
+                        root.showAlert(message: NSLocalizedString("update_message", comment: ""), buttonAction: {
+                            UIApplication.shared.open(appStoreManeger.appStoreURL)
+                        })
+                    }
+                }
+            }
+        }
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
